5797907758283c57d2e8522a01f72e0877887224
Popelnyk <popelnyk@icloud.com>
2023-01-31 01:28:08 +0100

    init

---
+++ app/app/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  app
+//
+//  Created by Alexander Popelnyk on 31.01.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "here we go\n";
+    
+    return 0;
+}

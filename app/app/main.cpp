5839756b8fa59c69a1f683c1b88ba3e7529a6dd0
Popelnyk <popelnyk@icloud.com>
2023-01-31 02:37:10 +0100

    start building skeleton

--- app/app/main.cpp
+++ app/app/main.cpp
@@ -1,3 +1,5 @@
+//  Write a program for finnding intersecting 3d triangles
+//  The number of triangles <= 1e6
 //
 //  main.cpp
 //  app
@@ -6,6 +8,33 @@
 //
 
 #include <iostream>
+#include <utility>
+
+
+class Point3D {
+public:
+    Point3D() = delete;
+    Point3D(int x, int y, int z): x_(x), y_(y), z_(z) { };
+private:
+    int x_, y_, z_;
+};
+
+
+// Line is presented as Ax+By+C=0
+class Line2D {
+public:
+    Line2D() = delete;
+private:
+    
+};
+
+class Line3D {
+    
+};
+
+// Must be described as planes (3 planes in space)
+// class Triangle3D { }
+
 
 int main(int argc, const char * argv[]) {
     std::cout << "here we go\n";
